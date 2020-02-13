5db15bc50f914734f0ba8a627be8ddd2231c5908
Jader de Andrade <jaderdeandrade@gmail.com>
2020-02-13 20:08:52 -0300

    Criação do Projeto

---
+++ src/main/java/com/jda/app/AppVendasApiApplication.java
@@ -0,0 +1,13 @@
+package com.jda.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVendasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVendasApiApplication.class, args);
+	}
+
+}
